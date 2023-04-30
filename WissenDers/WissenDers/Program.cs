bde54207e25fcabadcd7f93c833d71460098afc6
Doğukan Durkaya <98876145+dogukandurkaya@users.noreply.github.com>
2023-04-30 14:10:58 +0300

    Add files via upload

---
+++ WissenDers/WissenDers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WissenDers.wissen_egitim;
+
+namespace WissenDers
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Egitim nesne = new Egitim();
+            nesne.Ornekler();
+
+           // egitim.Ornekler();
+            //egitim.Test();
+        }
+    }
+}
---
+++ WissenDers/WissenDers/wissen_egitim/Egitim.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WissenDers.wissen_egitim
+{
+    public class Egitim
+    {
+        public void Ornekler()
+        {
+            //Metinsel
+            //string name = "Doğukan";
+            //Console.WriteLine(name);
+            //Console.ReadLine();
+
+            //string name2;
+            //name2 = "Durkaya";
+            //Console.WriteLine(name2 + "\n" + name);
+
+
+
+            //Sayısal
+            //byte number1 = 12;
+            //int number = -1;
+            //Console.WriteLine(number);
+
+
+
+            //Virgüllüler
+            //float number = 10.5f;
+            //double number2 = 10.5;
+            //decimal number3 = 10.5m;
+
+            //Console.WriteLine(number);
+            //Console.WriteLine(number2);
+            //Console.WriteLine(number3);
+
+            //char test = '1';
+            //Console.WriteLine(test);
+
+            //bool test = false;
+            //Console.WriteLine(test);
+
+
+            //int sayi1, sayi2;
+            //sayi1 = 15; sayi2= 25;
+            //sayi2 = 25;
+            //Console.WriteLine(sayi1 + sayi2);
+            //Toplama için +
+            //Çıkarma için -
+            //Çarpma için *
+            //Bölme / kullanılır.
+
+            //double sayi1, sayi2;
+            //sayi1 = 30;
+            //sayi2 = 40;
+            //Console.WriteLine("Toplam: " + (sayi1 + sayi2));
+            //Console.WriteLine("Çıkarma: " + (sayi1 - sayi2));
+            //Console.WriteLine("Çarpma: " + (sayi1 * sayi2));
+            //Console.WriteLine("Bölme: " + (sayi1 / sayi2));
+
+            //Bir ürün olsun. Fiyat 100 tl. KDV'li halini hesaplayalım. KDV = %18
+            //double price = 99;
+            //Console.WriteLine("KDVLİ FİYAT: " + ((price * 118) / 100));
+            //Console.WriteLine("KDVLİ FİYAT: " + (price * 1.18));
+            //Console.WriteLine($"{price}");
+            //Console.WriteLine("{price}");
+
+            //Console.Write("Lütfen Bir İsim Giriniz: ");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("Girilen isim: " + name);
+
+            //Console.Write("Bir sayı giriniz: ");
+            //int number;
+            //number = Convert.ToInt32(Console.ReadLine());
+            ////number = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Girilen sayı:" + number);
+
+
+
+            //Ekrandan 2 vize  bir final notu okutunuz. Vize ortalamasinin %30 u finalin % 70 ni alip ekrana bastrıalım
+            //45 67 ile 78
+
+            //double vize1, vize2, final, ortalama;
+            //Console.Write("1. Vize Giriniz: ");
+            //vize1 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("2. Vize Giriniz: ");
+            //vize2 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Final Giriniz: ");
+            //final = Convert.ToDouble(Console.ReadLine());
+            //ortalama = (((vize1 + vize2) / 2) * 0.3 + (final * 0.7));
+            //Console.WriteLine("Ortalama: " + ortalama);
+
+            //int number = 1;
+            //int number2 = 1;
+            //if (number == 1 && number2 == 1)
+            //{
+            //    Console.WriteLine("İf kısmı");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Else kısmı");
+            //}
+
+            //int number = 10;
+
+            //if (number == 9)
+            //{
+            //    Console.WriteLine("burası if kısmı");
+            //}
+            //else if (number == 8)
+            //{
+            //    Console.WriteLine("burası else if kısmı");
+            //}
+            //else if (number == 7)
+            //{
+            //    Console.WriteLine("2.  else if kısmı ");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Else kısmı");
+            //}
+
+            //switch (number)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Burası 1");
+            //        break;
+            //    case 2: Console.WriteLine("Burası 2");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Değer sonuçsuz");
+            //        break;
+            //}
+
+            //double vize1, vize2, final, ortalama;
+            //Console.Write("1. Vize Giriniz: ");
+            //vize1 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("2. Vize Giriniz: ");
+            //vize2 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Final Giriniz: ");
+            //final = Convert.ToDouble(Console.ReadLine());
+            //ortalama = (((vize1 + vize2) / 2) * 0.3 + (final * 0.7));
+            //Console.WriteLine("Ortalama: " + ortalama);
+
+
+            //if (ortalama >= 50)
+            //{
+            //    //console.writeline("tebrikler geçtiniz");
+            //    Console.WriteLine("malesef kaldınız");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("tebrikler geçtiniz");
+            //    //console.writeline("malesef kaldınız");
+            //}
+
+            // == EŞİT Mİ
+            // >= BÜYÜK EŞİT
+            // <= KÜÇÜK EŞİT
+            // != EŞİT DEĞİL
+            // && VE
+            // || VEYA
+
+            //Console.Write("Lütfen Yaşınızı Giriniz: ");
+            //int yas = Convert.ToInt32(Console.ReadLine());
+
+            //if (yas >= 18)
+            //{
+            //    Console.WriteLine("Ehliyet alabilirsiniz");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Ehliyet alamadınız");
+            //}
+
+
+
+            //switch (ortalama)
+            //{
+            //    case => 50:
+            //        Console.WriteLine("Geçtiniz");
+            //        break;
+            //    case < 50:
+            //        Console.WriteLine("Kaladınız");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Burası deafult");
+            //        break;
+            //}
+
+            Console.ReadLine();
+        }
+        //public void Test()
+        //{
+        //    Console.WriteLine("jsjsdajhasdjjasd");
+        //    Console.ReadLine();
+        //}
+    }
+}
